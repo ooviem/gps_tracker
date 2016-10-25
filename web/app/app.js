856e8bb2a7be3aa71a34c111cb111f3886780de5
Viem Ong <viemong@pingidentity.com>
2016-10-26 00:02:38 +0700

    no message

--- web/app/app.js
+++ web/app/app.js
@@ -11,15 +11,8 @@ angular.module('JackControllerApp', ['ngMaterial'])
 
             ctrlMe.map = function() {
                 callGET("/api/gps").then(function(data){
-                    var uluru = {lat: data.lat, lng: data.lng};
-                    var map = new google.maps.Map(document.getElementById('map'), {
-                      zoom: 8,
-                      center: uluru
-                    });
-                    var marker = new google.maps.Marker({
-                      position: uluru,
-                      map: map
-                    });
+                    ctrlMe.lat = data.lat;
+                    ctrlMe.lng = data.lng;
                 });
             };
             ctrlMe.map();
