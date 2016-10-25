599cb3e9183d162dfa0091bd6c7694ad6f4ae129
Viem Ong <viemong@pingidentity.com>
2016-10-25 21:15:20 +0700

    no message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var WebServer = require("./web/server.js");
 var gps = require("./command.js");
 var mainFunction = function(){
-   	var command = "echo B0";
+   	var command = "sudo stty -F /dev/ttyUSB0 ispeed 4800 && cat < /dev/tUSB0";
 
     gps.exe(command).then(function(data){
     	console.log(data);
