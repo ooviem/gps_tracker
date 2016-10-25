2947ddea9bae9db60110ed1f047a68f303e8ec37
Viem Ong <viemong@pingidentity.com>
2016-10-25 21:45:33 +0700

    no message

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function gotData(data){
 	var array = data.split(",");
 	switch (array[0]) {
 		case "$GPGLL":
-			longtitude = (array[0] !== "")? array[1] : longtitude;
-			latitude = (array[0] !== "")? array[3] : latitude;
+			longtitude = (array[0] !== "")? array[1] : longtitude+ "N";
+			latitude = (array[0] !== "")? array[3] : latitude+"E";
 	}
 	console.log(longtitude + " " + latitude);
 }   
