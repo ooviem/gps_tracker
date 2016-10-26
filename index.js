30891f8184df2078737d44ccc20bfc01ebf474f9
Viem Ong <viemong@pingidentity.com>
2016-10-26 20:26:35 +0700

    no message

--- index.js
+++ index.js
@@ -42,7 +42,46 @@ app.get('/api/gps', function (req, res) {
     	"lng": longtitude
     });
 });
+app.get('/api/gps', function (req, res) {
+       var https = require('https');
+		var data = JSON.stringify({
+		 api_key: '6cdcc83f',
+		 api_secret: '541cd43f233e8ca4',
+		 to: '841234555864',
+		 from: '841234555864',
+		 text: 'SOS, Phát hiện tình huống nguy hiểm, vui lòng xem tại https://www.google.com/maps/place/'+window.lat+'N'+window.lng+'E'
+		});
 
+		var options = {
+		 host: 'rest.nexmo.com',
+		 path: '/sms/json',
+		 port: 443,
+		 method: 'POST',
+		 headers: {
+		   'Content-Type': 'application/json',
+		   'Content-Length': Buffer.byteLength(data)
+		 }
+		};
+
+		var req = https.request(options);
+
+		req.write(data);
+		req.end();
+
+		var responseData = '';
+		req.on('response', function(res){
+		 res.on('data', function(chunk){
+		   responseData += chunk;
+		 });
+
+		 res.on('end', function(){
+		   console.log(JSON.parse(responseData));
+		 });
+		});
+    res.json({
+    	"OK":"OK"
+    });
+});
 
  /* serves all the static files */
  app.get(/^(.+)$/, function(req, res){ 
--- web/app/app.js
+++ web/app/app.js
@@ -16,40 +16,10 @@ angular.module('JackControllerApp', ['ngMaterial'])
                 });
             };
             ctrlMe.send = function() {
-                var https = require('https');
-                var data = JSON.stringify({
-                 api_key: '6cdcc83f',
-                 api_secret: '541cd43f233e8ca4',
-                 to: '841234555864',
-                 from: '841234555864',
-                 text: 'SOS, Phát hiện tình huống nguy hiểm, vui lòng xem tại https://www.google.com/maps/place/'+window.lat+'N'+window.lng+'E'
-                });
-
-                var options = {
-                 host: 'rest.nexmo.com',
-                 path: '/sms/json',
-                 port: 443,
-                 method: 'POST',
-                 headers: {
-                   'Content-Type': 'application/json',
-                   'Content-Length': Buffer.byteLength(data)
-                 }
-                };
-
-                var req = https.request(options);
-
-                req.write(data);
-                req.end();
-
-                var responseData = '';
-                req.on('response', function(res){
-                 res.on('data', function(chunk){
-                   responseData += chunk;
-                 });
-
-                 res.on('end', function(){
-                   console.log(JSON.parse(responseData));
-                 });
+                callGET("/api/sms").then(function(res){
+                    if(res.data === "OK"){
+                        alert("Gởi thành công");
+                    }
                 });
             };
             ctrlMe.map();
