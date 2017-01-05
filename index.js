94c8241736226dedeb591a625cbe3ff1f2593018
viemong <viem.oo22992@gmail.com>
2017-01-05 18:33:03 +0700

    no message

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ var gpio = require('rpi-gpio');
 
 gpio.setup(7, gpio.DIR_IN, readInput);
 
- 
-function readInput() {
-    gpio.read(7, function(err, value) {
-        console.log('The value is ' + value);
-    });
-}
+
 
 serialjs.open(
     '/dev/ttyUSB0',
@@ -147,3 +142,9 @@ var server = app.listen(80, function () {
 })
 mainFunction();
 WebServer.initWebServer();
+
+function readInput() {
+    gpio.read(7, function(err, value) {
+        console.log('The value is ' + value);
+    });
+}
