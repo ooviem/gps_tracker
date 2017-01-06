7657c68fb8dc912e14d7f53a56d2c368c482b129
viemong <viem.oo22992@gmail.com>
2017-01-06 15:14:47 +0700

    no message

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function gotData(data){
 serialjs.open(
     '/dev/ttyACM0',
     start2,
-    '\n'
+	''
 );
 
 function start2(port){
@@ -66,14 +66,7 @@ function start2(port){
 
 function gotData2(data){
 	keyboard += data[0];
-	console.log(data[0]);
-	console.log(data[1]);
-	console.log(data[2]);
-	console.log(data[3]);
-	console.log(data[4]);
-	console.log(data[5]);
-	console.log(data[6]);
-
+	console.log(data);
 };  
 
 
