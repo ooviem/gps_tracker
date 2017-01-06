a0d18cb5837c009c1802cee6d3ccf348c0768c59
viemong <viem.oo22992@gmail.com>
2017-01-06 15:38:00 +0700

    no message

--- index.js
+++ index.js
@@ -65,8 +65,7 @@ function start2(port){
 };
 
 function gotData2(data){
-	keyboard += data[0];
-	console.log(data);
+	console.log(data.trim());
 };  
 
 
@@ -107,12 +106,12 @@ function sendVNSMS(content, number){
 		   		console.log(JSON.parse(responseData));
 		 	});
 		});
-}
+};
 
- /* serves main page */
- app.get("/", function(req, res) {
+/* serves main page */
+app.get("/", function(req, res) {
     res.sendfile('./web/index.html')
- });
+});
 
 app.get('/api/gps', function (req, res) {
     res.json({
@@ -122,53 +121,22 @@ app.get('/api/gps', function (req, res) {
 });
 
 app.get('/api/sms', function (req, res) {
-       var https = require('https');
-		var data = JSON.stringify({
-		 api_key: '81c52516',
-		 api_secret: '1d168d66fe00d0ca',
-		 to: '84905334613',
-		 from: '841234555864',
-		 text: 'SOS, Nguoi than cua ban hien gap nguy hiem, xin xem tai https://www.google.com/maps/place/'+latitude+'N'+longtitude+'E'
-		});
-
-		var options = {
-		 host: 'rest.nexmo.com',
-		 path: '/sms/json',
-		 port: 443,
-		 method: 'POST',
-		 headers: {
-		   'Content-Type': 'application/json; charset=utf-8',
-		   'Content-Length': Buffer.byteLength(data)
-		 }
-		};
-
-		var req = https.request(options);
-
-		req.write(data);
-		req.end();
-
-		var responseData = '';
-		req.on('response', function(res){
-		 res.on('data', function(chunk){
-		   responseData += chunk;
-		 });
-
-		 res.on('end', function(){
-		   console.log(JSON.parse(responseData));
-		 });
-		});
+    sendVNSMS('SOS, Nguoi than cua ban hien gap nguy hiem, xin xem tai https://www.google.com/maps/place/'+latitude+'N'+longtitude+'E', "01234555864");
     res.json({
     	"OK":"OK"
     });
 });
 app.get('/api/sms2', function (req, res) {
        sendVNSMS('Thiet bi dang dich chuyen, vi tri hien tai https://www.google.com/maps/place/'+latitude+'N'+longtitude+'E', "01234555864");
+	res.json({
+    	"OK":"OK"
+    });
 });
 
  /* serves all the static files */
- app.get(/^(.+)$/, function(req, res){ 
-     res.sendfile( __dirname +"/web/"+ req.params[0]); 
- });
+app.get(/^(.+)$/, function(req, res){ 
+    res.sendfile( __dirname +"/web/"+ req.params[0]); 
+});
 
 
 var server = app.listen(80, function () {
