e63f07d97b824b5af28ff9d18d54cd732e0c417c
Viem Ong <viem.oo22992@gmail.com>
2017-02-27 20:53:32 +0700

    no message

--- index.js
+++ index.js
@@ -205,23 +205,24 @@ function start(port){
 function gotData(data){
 	var array = data.split(",");
 	var line = array[0];
-	try {
-        var packet = nmea.parseNmeaSentence(line);
+	console.log(line);
+	// try {
+ //        var packet = nmea.parseNmeaSentence(line);
  
-        if (packet.sentenceId === "RMC" && packet.status === "valid") {
-            console.log("Got location via RMC packet:", packet.latitude, packet.longitude);
-        }
+ //        if (packet.sentenceId === "RMC" && packet.status === "valid") {
+ //            console.log("Got location via RMC packet:", packet.latitude, packet.longitude);
+ //        }
  
-        if (packet.sentenceId === "GGA" && packet.fixType !== "none") {
-            console.log("Got location via GGA packet:", packet.latitude, packet.longitude);
-        }
+ //        if (packet.sentenceId === "GGA" && packet.fixType !== "none") {
+ //            console.log("Got location via GGA packet:", packet.latitude, packet.longitude);
+ //        }
  
-        if (packet.sentenceId === "GSA") {
-            console.log("There are " + packet.satellites.length + " satellites in view.");
-        }
-    } catch (error) {
-        console.error("Got bad packet:", line, error);
-    }
+ //        if (packet.sentenceId === "GSA") {
+ //            console.log("There are " + packet.satellites.length + " satellites in view.");
+ //        }
+ //    } catch (error) {
+ //        console.error("Got bad packet:", line, error);
+ //    }
 	// var array = data.split(",");
 	// switch (array[0]) {
 	// 	case "$GPGLL":
